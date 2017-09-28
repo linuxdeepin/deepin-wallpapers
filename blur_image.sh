cf06200a046e1f962a7d33029e619764e597449f
deepinzhangshuang <zhangshuang@linuxdeepin.com>
2017-09-28 16:54:27 +0800

    change to image-blur
    Change-Id: I4b4f770f9ece9e2491c7fa5d2699e7b9b7f1894c

--- blur_image.sh
+++ blur_image.sh
@@ -6,5 +6,5 @@ do
 	md5name=`echo -n /usr/share/wallpapers/deepin/$inname | md5sum`
 	md5name=${md5name%  *}
 	outname=$md5name.jpg
-	/usr/lib/deepin-api/image-blur-helper ./deepin/$inname ./image-blur/$outname
+	/usr/lib/deepin-api/image-blur --sigma 30 ./deepin/$inname ./image-blur/$outname
 done
