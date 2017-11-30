25d1d16b580c6b0932fc6113701b11bd88409046
deepinzhangshuang <zhangshuang@linuxdeepin.com>
2017-11-30 13:34:53 +0800

    add pappers for deepin-private
    Change-Id: I3a51bb30b931ed86cec076959b9f47e2aff622e3

--- blur_image.sh
+++ blur_image.sh
@@ -8,3 +8,11 @@ do
 	outname=$md5name.jpg
 	/usr/lib/deepin-api/image-blur --sigma 30 ./deepin/$inname ./image-blur/$outname
 done
+for inname in ./deepin-private/*
+do
+	inname=${inname##*/}
+	md5name=`echo -n /usr/share/wallpapers/deepin/$inname | md5sum`
+	md5name=${md5name%  *}
+	outname=$md5name.jpg
+	/usr/lib/deepin-api/image-blur --sigma 30 ./deepin-private/$inname ./image-blur/$outname
+done
